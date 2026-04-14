181463d162812e1fc851123d516f50cbdb213504
Moustapha Merhi <mmerhi2@wgu.edu>
2026-04-13 20:27:26 -0400

    Fix discount item selector

--- wallyz3/supabase/migrations/20260321000002_add_must_change_password 2.sql
+++
@@ -1,7 +0,0 @@
--- Add must_change_password column to admin_users
-ALTER TABLE admin_users ADD COLUMN IF NOT EXISTS must_change_password boolean DEFAULT false;
-
--- Make sure testnetwork61@gmail.com is in approved_admins
-INSERT INTO approved_admins (email, full_name, is_active)
-VALUES ('testnetwork61@gmail.com', 'Super Admin', true)
-ON CONFLICT (email) DO UPDATE SET is_active = true;
