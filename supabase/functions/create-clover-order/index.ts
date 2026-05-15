aeba8147d8cac8e3f2b32c36643458f22d9ca4b3
Moustapha Merhi <mmerhi2@wgu.edu>
2026-05-15 17:39:51 -0400

    send discounts as object not array

--- supabase/functions/create-clover-order/index.ts
+++ supabase/functions/create-clover-order/index.ts
@@ -102,9 +102,10 @@ serve(async (req) => {
         phoneNumber: customer_phone,
       },
       shoppingCart: {
-        lineItems: discountCents > 0
-          ? [...lineItems, { name: 'Promo discount', price: -discountCents, unitQty: 1 }]
-          : lineItems,
+        lineItems,
+        ...(discountCents > 0 ? {
+          discounts: { name: 'Promo discount', amount: discountCents }
+        } : {}),
       },
     };
 
